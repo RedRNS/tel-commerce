a6758e0701812463310b0d320299d882b0d9cf4d
RedRNS <putrirenisa@gmail.com>
2025-08-15 08:16:31 +0700

    ganti

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -10,18 +10,18 @@ const Logo = ({ className = "" }: LogoProps) => {
   return (
     <div className={`flex items-center ${className}`}>
       {/* Logo Image - ganti src ini dengan path logo Tel-Commerce yang sebenarnya */}
-      <div className="w-10 h-10 bg-white rounded-lg flex items-center justify-center mr-3">
-        {/* Placeholder logo - akan diganti dengan <Image> component */}
-        <span className="text-red-600 text-xl font-bold">T</span>
-        
-        {/* Uncomment dan edit ini ketika logo sudah tersedia:
+      <div className="w-10 h-10 rounded-lg flex items-center justify-center mr-3">
+        {/* Logo Image - ganti src ini dengan path logo Tel-Commerce yang sebenarnya */}
         <Image
-          src="/images/telcommerce-logo.png"
+          src="/images/logo.png"
           alt="Tel-Commerce Logo"
           width={40}
           height={40}
           className="rounded-lg"
         />
+        
+        {/* Placeholder logo - akan diganti dengan <Image> component
+        <span className="text-red-600 text-xl font-bold">TC</span>
         */}
       </div>
       <span className="text-white text-2xl font-bold">Tel-Commerce</span>
